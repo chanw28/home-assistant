1bba444f86bd304a576ecd4a059a77efd8c27a19
chanw28 <chanw28@yahoo.com>
2021-10-01 03:49:00 +1000

    v0.1

--- discover_devices/rootfs/opt/discover_device.py
+++ discover_devices/rootfs/opt/discover_device.py
@@ -2,10 +2,7 @@
  
 import os
 
-msg = "Hello World2b from python run2.py!"
+msg = "Hello World2b from python3 discover_devices.py"
 print(msg)
 
 os.system("ping -c 1 192.168.0.230")
-
-#logger.info(msg)
-#hass.bus.fire(name, {"wow": "from a Hello_World2b run2.py"})
